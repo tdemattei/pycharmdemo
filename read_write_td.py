1004482d5cb0ac743bb3e062ea9d79f4c8e6d10c
4460 <tdemattei@suncresthomes.com>
2019-10-17 13:48:26 -0700

    Added one line

---
+++ read_write_td.py
@@ -0,0 +1,10 @@
+# Learn code in read_write.py
+
+
+import os  # import built in os module
+
+os_info = os.name  # Set os_info = to os.name
+print(os_info)  # Print os_info
+
+if os.name == 'nt':  # Test if os is equal to nt
+    print("True")  # If os is equal to nt print True
