61476e0ca5a608d6c3ab6e5a6ba05189ce75372b
4460 <tdemattei@suncresthomes.com>
2019-11-04 23:34:19 -0800

    Still trying to get gamepad working. Return statements breaking out of loops

--- Test Input 02.py
+++ Test Input 02.py
@@ -19,54 +19,59 @@ out_max = (600)
 def gp_input():
 
 
-    global velocity
-    while 1:
-        events = get_gamepad()
-        if events:
-            for event in events:
-                if event.code == "BTN_START" and event.state == 1:
-                    print("Start Button Pressed")
-                    return
+    velocity = None
+    # while 1:
+    events = get_gamepad()
+    if events:
+        print("if events")
+        for event in events:
+            if event.code == "BTN_START" and event.state == 1:
+                print("Start Button Pressed")
+                return
 
-                elif event.code != "ABS_RX":
-                    print("this is break")
-                    break
-                elif event.code == "ABS_RX" and abs(event.state) > in_min and abs(event.state) <= in_max:
-                    velocity = round((abs(event.state) - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
-                    print("+++++++++++++++++++++++++++++++++++++")
-                    print("Velocity Positive " + str(velocity))
-                    print("++++++++++++++++++++++++++++++++++++++")
-                    # print(velocity)
-                    if event.state < 0:
-                        velocity = velocity * -1
-                        print("-----------------------------------")
-                        print("Velocity Negative " + str(velocity))
-                        print("------------------------------------")
-                        # print(event.ev_type, event.code, event.state)
-                        # print(velocity)
-                elif event.code == "ABS_RX":
-                    velocity = 0
-                    print("000000000000000000000000000000000000000")
-                    print("Velocity Zero " + str(velocity))
-                    print("000000000000000000000000000000000000000")
+            elif event.code != "ABS_RX":
+                print("this is break")
+                break
+            elif event.code == "ABS_RX" and abs(event.state) > in_min and abs(event.state) <= in_max:
+                velocity = round((abs(event.state) - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+                print("+++++++++++++++++++++++++++++++++++++")
+                print("Velocity Positive " + str(velocity))
+                print("++++++++++++++++++++++++++++++++++++++")
+                # print(velocity)
+                if event.state < 0:
+                    velocity = velocity * -1
+                    print("-----------------------------------")
+                    print("Velocity Negative " + str(velocity))
+                    print("------------------------------------")
                     # print(event.ev_type, event.code, event.state)
                     # print(velocity)
-
-                # print("=====================================")
+            elif event.code == "ABS_RX":
+                velocity = 0
+                print("000000000000000000000000000000000000000")
+                print("Velocity Zero " + str(velocity))
+                print("000000000000000000000000000000000000000")
                 # print(event.ev_type, event.code, event.state)
-                # print("this is loop 2")
-                print("Velocity = " + str(velocity))
-                # print("======================================")
-                return velocity
-        else:
-            print("this is no event")
+                # print(velocity)
 
-        print("While 1")
-        print("While 2")
-        print("While 3")
+            print("=====================================")
+            print(event.ev_type, event.code, event.state)
+            print("this is loop 2")
+            print("Velocity = " + str(velocity))
+            print("======================================")
+            return (velocity)
+        print("All Return Here")
+    else:
+        print("this is no event")
 
-gp_input()
+    print("While 1")
+    print("While 2")
+    print("While 3")
+    print(gp_input())
+
+while 1:
+
+    gp_input()
 
 print(gp_input())
-print(velocity)
+# print(velocity)
 print("bye")
